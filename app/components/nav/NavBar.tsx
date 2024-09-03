ccdd53b41a3434bad838cab8848ee10a385399b1
gabrielnavas <gabrielnavas@protonmail.com>
2024-09-03 20:32:39 -0300

    create the footer and the navbar

---
+++ app/components/nav/NavBar.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+interface INavBarProps { };
+
+export const NavBar: FC<INavBarProps> = (props) => {
+  return (
+    <div>
+      NavBar
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      E-Shop
-    </main>
+    <div>
+      E-Shopa
+    </div>
   )
 }
