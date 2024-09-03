6e861393bc777666fde58a60a231a7f49e5f7dae
gabrielnavas <gabrielnavas@protonmail.com>
2024-09-03 20:09:46 -0300

    basic nextjs project started

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] })
+
+export const metadata: Metadata = {
+  title: 'E-Shop',
+  description: 'E-Commerce App',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
