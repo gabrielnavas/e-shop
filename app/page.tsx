474b433134b604d57314a7c54b9b04172b0bf431
gabrielnavas <gabrielnavas@protonmail.com>
2024-09-03 23:07:42 -0300

    product grid

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,17 @@ export default function Home() {
         <div>
           <HomeBanner />
         </div>
-        <div>
+        <div 
+          className="
+            grid
+            grid-cols-2
+            sm:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
           {products.map(product => (
             <div>{truncateText(product.name)}</div>
           ))}
